0eac5d9ca5944580ff378ea055b5ea2ef5e4ad5e
rjarsenault1101 <rjarsenault1101@gmail.com>
2020-04-04 13:29:17 -0400

    Added even more things

--- data/models.py
+++ data/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Datapoint(models.Model):
+    submit_date = models.DateTimeField(default=datetime.now, blank=False)
+    value = models.BigIntegerField(blank=False)
+    # Add in a 'which fountain button'? 
+    def __str__(self):
+        return str(self.value)
--- data/views.py
+++ data/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Datapoint as dp
 
 def index(request): 
     return render(request, 'pages/index.html')
+
+def new_data(request):
+    if request.method == 'POST':
+        data = request.POST['datapoint']
+        #if data < dp.objects.order_by('-id')[0]['value']:
+        datapoint = dp(value=data)
+        datapoint.save()
+    return redirect('index')
